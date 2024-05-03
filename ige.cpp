b1e3e20d7e21f3ed8cd4f8c7fdebd8b51d412423
jigul123 <168332798+jigul123@users.noreply.github.com>
2024-05-04 00:46:00 +0500

    Add files via upload

---
+++ ige.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	string str;
+	for (int i = 0; i < n; i++) {
+		str = str + "*";
+		cout << str << endl;
+	}
+}
+
